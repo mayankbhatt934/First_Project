7f38dc9c8b8b8e3a908e31260a18c51f74463a7b
Mayank <mayankbhatt934@gmail.com>
2025-09-18 22:12:49 +0530

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,9 +1,6 @@
 # include <stdio.h> 
 
 int main () {
-    printf("Hello, World!!\n My name is Mayank.");
+    printf("Hello, World!!\n My name is Mayank and I am currently pursuing Bachelor of Computer Application at Galgotias University.");
     return 0;
 }
-
-// "\n" creates a new line.
-// ./a.exe is a run command to run a code.
