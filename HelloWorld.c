fc54e0e4ad80b3d3c3a0420663da9239f303dbde
Mayank <mayankbhatt934@gmail.com>
2025-09-18 22:11:41 +0530

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+# include <stdio.h> 
+
+int main () {
+    printf("Hello, World!!\n My name is Mayank.");
+    return 0;
+}
+
+// "\n" creates a new line.
+// ./a.exe is a run command to run a code.
